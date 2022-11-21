de37d1ae985a26b84f39068d625154271e95ac9f
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-11-21 16:19:27 -0300

    refactor: change decorators to native args

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,16 +1,9 @@
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { Meta, Story } from '@storybook/react';
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text('Description', 'Descrição do storybook')}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
