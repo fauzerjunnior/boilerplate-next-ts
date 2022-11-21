c37a667e32d9eb804037429cd1adad080f986915
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-11-21 17:09:29 -0300

    refactor: update pwa config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = withPWA({
+  dest: 'public',
+  disable: !isProd,
+});
